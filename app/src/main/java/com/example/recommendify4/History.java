908c92819eeb160ecdb4ae54111574042f2a90fa
Morata <alvarosangar.x2@gmail.com>
2020-11-08 21:29:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recommendify4/History.java
@@ -0,0 +1,54 @@
+package com.example.recommendify4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        //Initializa and Assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set home selected
+        bottomNavigationView.setSelectedItemId(R.id.history);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.advanced:
+                        startActivity(new Intent(getApplicationContext()
+                                , Advanced.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                , MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.history:
+                        startActivity(new Intent(getApplicationContext()
+                                , History.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                }
+                return false;
+
+            }
+        });
+
+
+    }
+}
