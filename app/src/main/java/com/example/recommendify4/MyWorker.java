e3dcb5771dcb050a8bc2eaa61793f85724580913
pablogarciastc <infopablogs@gmail.com>
2020-12-13 20:20:57 +0100

    Integration of Content-based Recommendator

--- app/src/main/java/com/example/recommendify4/MyWorker.java
+++ app/src/main/java/com/example/recommendify4/MyWorker.java
@@ -31,6 +31,7 @@ public class MyWorker extends Worker {
 
 
     private Context context;
+
     public MyWorker(
             @NonNull Context context,
             @NonNull WorkerParameters params) {
@@ -38,27 +39,18 @@ public class MyWorker extends Worker {
         this.context = context;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @NonNull
     @Override
     public Result doWork() {
         String TopSongs = getInputData().getString(CANCIONES);
-    /*    String idValidSong = null;
-        try {
-            idValidSong = GetValidSong(TopSongs);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("IdValidSong: " + idValidSong);*/
-
         String[] songs = TopSongs.split("Song |Song |Song|Song |Song|Song|Song|Song|Song");
         if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(context));
         }
-        Python py= Python.getInstance();
+        Python py = Python.getInstance();
         PyObject pyf = py.getModule("FinalRecomendator");
 
-        PyObject obj= pyf.callAttr("rank_song_similarity_by_measure",TopSongs,2);
+        PyObject obj = pyf.callAttr("rank_song_similarity_by_measure", TopSongs, 2);
         Data outputData = new Data.Builder()
                 .putString(KEY_RESULT, obj.toString())
                 .build();
@@ -71,78 +63,31 @@ public class MyWorker extends Worker {
 
     }
 
-    public String GetArtist(String TopSongs,int i){
+    public String GetArtist(String TopSongs, int i) {
         String[] songs = TopSongs.split("Song |Song |Song|Song |Song|Song|Song|Song|Song");
         System.out.println("Songs: " + TopSongs);
         System.out.println("Song: " + songs[i]);
         String[] Artists = songs[i].split("=");
         String[] FinalArtist = Artists[5].split(",");
-        FinalArtist[0] = FinalArtist[0].substring( 1, FinalArtist[0].length() - 1 );
+        FinalArtist[0] = FinalArtist[0].substring(1, FinalArtist[0].length() - 1);
         return FinalArtist[0];
     }
-    public String GetSong(String TopSongs,int i){
+
+    public String GetSong(String TopSongs, int i) {
         String[] songs = TopSongs.split("Song |Song |Song|Song |Song|Song|Song|Song|Song");
         String[] SongTitle = songs[i].split("name|=");
         String[] FinalSong = SongTitle[2].split(",");
-        FinalSong[0] = FinalSong[0].substring( 1, FinalSong[0].length() - 1 );
+        FinalSong[0] = FinalSong[0].substring(1, FinalSong[0].length() - 1);
         return FinalSong[0];
     }
-    public String GetId(String TopSongs,int i){
+
+    public String GetId(String TopSongs, int i) {
         String[] songs = TopSongs.split("Song |Song |Song|Song |Song|Song|Song|Song|Song");
         String[] ID = songs[i].split("=");
         String[] FinalID = ID[3].split(",");
-        FinalID[0] = FinalID[0].substring( 1, FinalID[0].length() - 1 );
+        FinalID[0] = FinalID[0].substring(1, FinalID[0].length() - 1);
 
         return FinalID[0];
     }
 
-   /* @RequiresApi(api = Build.VERSION_CODES.O)
-    public String GetValidSong(String TopSongs) throws IOException {
-        int getOut = 0;
-        String Txt = readFileAsString();
-        String[] ids = Txt.split(",");
-        String id = "";
-
-        while(getOut==0){
-            for(int j = 1; j < 10; j++){
-                id = GetId(TopSongs,j);
-                for (int i = 0; i < ids.length; i++) {
-                    if (ids[i].equals(i)){
-                        String Artist = GetArtist(TopSongs,j);
-                        String Song = GetSong(TopSongs,j);
-                        System.out.println("Id: " + id);
-                        System.out.println("Title: " + Song);
-                        System.out.println("Artist " + Artist );
-                        getOut=1;
-
-                    }
-                 }
-            }
-        }
-
-
-        return id;
-    }
-
-   /* @RequiresApi(api = Build.VERSION_CODES.O)
-    public String readFileAsString() throws IOException {
-        String line="";
-        try {
-            System.out.println("SAAAAAAAAAAAAAAAAAAA");
-            String file = "res/raw/ids.txt";
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(file);
-        int size = is.available();
-        byte[] buffer = new byte[size];
-        is.read(buffer);
-        is.close();
-        line = new String(buffer, "UTF-8");
-            System.out.println("SAAAAAAAAAAAAAAAAAAAe");
-
-        } catch (IOException ex) {
-        ex.printStackTrace();
-        return null;
-    }
-        return line;
-
-    }*/
 }
