9b50b71259862538df0a4d188aeeb4bbd3d65417
Camilo Piñón Blanco <camipi2010@gmail.com>
2020-12-30 12:03:01 +0100

    Added Soulmate Artist fragment

---
+++ app/src/main/java/com/app/recommendify4/Fragments/FragmentSoulmateArtist.java
@@ -0,0 +1,120 @@
+package com.app.recommendify4.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.app.recommendify4.SpotifyItems.Artist;
+import com.bumptech.glide.Glide;
+import com.app.recommendify4.R;
+import com.app.recommendify4.SpotifyApi.RequestSender;
+import com.app.recommendify4.SpotifyApi.ResponseProcessor;
+import com.app.recommendify4.SpotifyItems.Song;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.app.recommendify4.ThreadManagers.ThreadLauncher;
+import com.app.recommendify4.UserInfo.Credentials;
+import com.app.recommendify4.UserInfo.UserRecommendations;
+
+
+
+public class FragmentSoulmateArtist extends Fragment {
+
+    private static final String ARTISTRECOMMENDED = "RecommendedSong";
+    private static final String CREDENTIALS = "Credentials";
+    private ArrayList<Artist> listOfRecommendations;
+
+    private Artist artist;
+    private Credentials credentials;
+
+    private TextView artistNameView;
+    private ImageView artistImage;
+
+
+    public FragmentSoulmateArtist() {
+        // Required empty public constructor
+    }
+
+    public static FragmentSoulmateArtist newInstance(ArrayList<Artist> artistsToRecommend, Credentials credentials) {
+        FragmentSoulmateArtist fragment = new FragmentSoulmateArtist();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARTISTRECOMMENDED, artistsToRecommend);
+        args.putParcelable(CREDENTIALS,credentials);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            listOfRecommendations = getArguments().getParcelableArrayList(ARTISTRECOMMENDED);
+            credentials = getArguments().getParcelable(CREDENTIALS);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_soulmate_artist, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton nextArtist = (ImageButton) view.findViewById(R.id.nextArtist);
+        nextArtist.setOnClickListener(v -> setNextSong());
+        artistImage = (ImageView) view.findViewById(R.id.artistImage);
+        artistNameView = (TextView) view.findViewById(R.id.artistName);
+        setNextSong();
+    }
+
+    public void setNextSong() {
+        artist = listOfRecommendations.get(0);
+
+        //Necessary to PUT it in the Historic artist list first...
+
+        listOfRecommendations.remove(artist);
+
+        ThreadLauncher builder_updateTrack = new ThreadLauncher();
+        builder_updateTrack.execute(new Runnable() {
+            @Override
+            public void run() {
+                String response = RequestSender.getTrackInfo(credentials, artist.getId());
+                ResponseProcessor.processTrackResponse(response, artist);
+            }
+        });
+
+        System.out.println("IMAGEN: " + artist.getCoverURL());
+
+        if(artist != null){
+            artistNameView.setText(artist.getName());
+            Glide.with(this).load(artist.getCoverURL()).into(artistImage);
+            try {
+                if(artist.getPrewviewURL() != null) playSOng();
+                else System.out.println("Song without preview URL");
+            } catch (IOException e) {
+                System.out.println("Error trying to play song");
+                e.printStackTrace();
+            }
+        }else{
+            artistNameView.setText("No more songs");
+        }
+    }
+
+
+
+}
--- app/src/main/python/3ArtistRecommender.py
+++ app/src/main/python/3ArtistRecommender.py
@@ -40,13 +40,13 @@ def recommend_artist(artist1, artist2, artist3):
         # sort
         match_tuple = sorted(match_tuple, key=lambda x: x[2])[::-1]
         if not match_tuple:
-            print(f"The recommendation system could not find a match for {song}")
+            print(f"The recommendation system could not find a match for {artist}")
 
         recom_artist_id = match_tuple[0][1]
         # Start the recommendation process
         print(f"Starting the recommendation process for {artist} ...")
         # Return the n neighbors for the song id
-        distances, indices = model_knn.kneighbors(wide_artist_data_sparse[recom_artist_id], n_neighbors=2+1)
+        distances, indices = model_knn.kneighbors(wide_artist_data_sparse[recom_artist_id], n_neighbors=3+1)
         recommendation_ids =sorted(list(zip(indices.squeeze().tolist(), distances.squeeze().tolist())), key=lambda x: x[1])[:0:-1]
 
 
@@ -54,10 +54,13 @@ def recommend_artist(artist1, artist2, artist3):
         recommendations_map = {artist_id: artist_name for artist_name, artist_id in decode_id_artist.items()}
         # Translate this recommendations into the ranking of song titles recommended
         for i, (idx, dist) in enumerate(recommendation_ids):
-            recommendations.append(recommendations_map[idx])
+            if recommendations_map[idx] not in recommendations and recommendations_map[idx] not in artist_array:
+              recommendations.append(recommendations_map[idx])
 
 
-    print(recommendations)
+    #print(recommendations)
     
     return ', '.join(recommendations)
 
+
+print(recommend_artist("Feng Suave", "Boy Pablo", "Arctic Monkeys"))
